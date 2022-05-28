8c02c44b23cb46e85e1b9e05c8e29515385ec7a6
Kubases <mokwajak@gmail.com>
2022-05-28 12:30:34 +0200

    finish_corrected

--- main.py
+++ main.py
@@ -20,24 +20,38 @@ def get_random_word(word_list):
     return random.choice(word_list)
 
 
+def convert_list_to_string(word):
+    new = ''
+    for letter in word:
+        new += letter
+    return new
+
+
 def game_end(player_won, game_word):
     if player_won:
-        print(f"Player won, the word was", end='  ')
+        print(f"Player won, the word was:", end=' ')
     else:
-        print(f"Pc won, the word was", end='  ')
-    print(*game_word)
+        print(f"Pc won, the word was:", end=' ')
+    print(convert_list_to_string(game_word))
 
 
 def game_round(num_of_rounds, game_word, player_word):
     round_num = 0
     guessed = 0
+    print_past_letter_flag = 0
+    print(*player_word)
+    past_letter = list()
     while round_num < num_of_rounds:
+        if print_past_letter_flag != 0:
+            print("Previous letters: ", end="")
+            print(*past_letter)
         if '_' not in player_word:
             player_won = 1
             game_end(player_won, game_word)
             break
         round_guess = get_guess(game_word, num_of_rounds, round_num)
         round_guess = round_guess.lower()
+        past_letter.append(round_guess)
         if len(round_guess) == 1:
             for index, letter in enumerate(game_word):
                 if letter == round_guess:
@@ -46,6 +60,7 @@ def game_round(num_of_rounds, game_word, player_word):
             if guessed:
                 guessed = 0
                 print(*player_word)
+                print_past_letter_flag =1
                 continue
         else:
             if list(round_guess) == game_word:
@@ -53,6 +68,7 @@ def game_round(num_of_rounds, game_word, player_word):
                 game_end(player_won, game_word)
                 break
         print(*player_word)
+        print_past_letter_flag = 1
         round_num += 1
 
 
