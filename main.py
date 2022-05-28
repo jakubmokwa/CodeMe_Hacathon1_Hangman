feddcabeb2b6ce18e2d479b067b999fe5187dfd0
Kubases <mokwajak@gmail.com>
2022-05-28 11:59:10 +0200

    finish

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def check_guess(word, game_word):
     return False
 
 
-def get_guess(game_word):
+def get_guess(game_word, num_of_rounds, round_num):
     while True:
-        word = input("Write your letter or guess the word (must be the same amount of letters) -> ")
+        word = input(f"Write your letter or guess the word ({num_of_rounds - round_num} guesses left) -> ")
         if check_guess(word, game_word):
             return word
         else:
@@ -21,15 +21,31 @@ def get_random_word(word_list):
 
 
 def game_round(num_of_rounds, game_word, player_word):
-    for i in range(num_of_rounds):
-        round_guess = get_guess(game_word)
+    round_num = 0
+    while round_num < num_of_rounds:
+        round_guess = get_guess(game_word, num_of_rounds, round_num)
+        round_guess = round_guess.lower()
+        if len(round_guess) == 1:
+            for index, letter in enumerate(game_word):
+                if letter == round_guess:
+                    player_word[index] = round_guess
+                    round_num -= 1
+            if '_' not in player_word:
+                print("Player won")
+                break
+        else:
+            if list(round_guess) == game_word:
+                print("Player won")
+                break
+        round_num += 1
+        print(*player_word)
 
 
 def main():
     word_list = ['pizza', 'empire', 'stellar', 'starfish', 'supremacy', 'federation', 'origin', 'battleship']
     num_of_rounds = 12
-    game_word = get_random_word(word_list)
-    player_word = ['_' * len(game_word)]
+    game_word = list(get_random_word(word_list))
+    player_word = list('_' * len(game_word))
     game_round(num_of_rounds, game_word, player_word)
 
 
